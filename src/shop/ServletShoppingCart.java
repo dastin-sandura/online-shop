c6c95aa901c96246eb9fa9cc281653dd16254967
Dastin Sandura <d.kuwalek@gmail.com>
2023-01-25 20:10:07 +0100

    Refactor class ServletShoppingCart
     - add few TODO tasks to investigate

--- src/shop/ServletShoppingCart.java
+++ src/shop/ServletShoppingCart.java
@@ -17,24 +17,34 @@ public class ServletShoppingCart extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		HttpSession s = req.getSession();
-		Map<Integer, ShoppingCartEntry> shoppingCart = (Map<Integer,ShoppingCartEntry>)s.getAttribute("shopping-cart");
+		HttpSession session = req.getSession();
+		//TODO introduce class ShoppingCart which will wrap the items in the shopping cart
+		Map<Integer, ShoppingCartEntry> shoppingCart = (Map<Integer,ShoppingCartEntry>)session.getAttribute("shopping-cart");
 		if (shoppingCart == null) {
 			shoppingCart = new HashMap<>();
 		}
-		Integer productId = new Integer(req.getParameter("id"));
+		Integer purchasedProductId = new Integer(req.getParameter("id"));
 		//a += req.getParameter("product");
-		ShoppingCartEntry entry = shoppingCart.get(productId);
+		//TODO use a service for checking if the Product is already in the shopping cart
+		ShoppingCartEntry entry = shoppingCart.get(purchasedProductId);
 
-		if (entry!=null) {
+		if (entry!=null) { // customer already has this type of product in the shopping cart
 			entry.setQuantity(entry.getQuantity()+1);
-		} else {
-			Product p = ShopProductsListDatabase.getProductById(productId);
+		} else { // customer does not have this product in the shopping cart
+			Product p = ShopProductsListDatabase.getProductById(purchasedProductId);
 			if (p!=null) {
-				shoppingCart.put(productId, new ShoppingCartEntry(1,p));
+				shoppingCart.put(purchasedProductId, new ShoppingCartEntry(1,p));
 			}
 		}
-		s.setAttribute("shopping-cart", shoppingCart);
+
+		//Update the session with updated shopping cart
+		//TODO, check if setting this attribute is necessary -
+		// maybe modifying the Map has already modified the object in the session
+		// if there is no need to set it again, then I should set this attribute only in case when I am creating new
+		// Map for storing the shopping cart.
+		// This would reduce the number of #setAttribute method calls from O(n) to O(1)
+
+		session.setAttribute("shopping-cart", shoppingCart);
 
 		PrintWriter p = resp.getWriter();
 		p.println("<html>");
